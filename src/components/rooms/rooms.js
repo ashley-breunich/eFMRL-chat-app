c3a670eabcceee81794223663dca5fffc29f541a
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-02-11 21:56:15 -0800

    got the CSS styling changed for smaller screen sizes

--- src/components/rooms/rooms.js
+++ src/components/rooms/rooms.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-let currentRooms = ['general', 'sports', 'coding'];
+let currentRooms = ['general', 'sports', 'coding', 'fashion'];
 
 const Rooms = props => {
     return (
       <>
         <h2 className='roomTitle'>Rooms</h2>
+        <div className="buttonGroup">
             {Object.keys(currentRooms).map((room, idx) => {
             return (
-                <button key={currentRooms[idx]} value={currentRooms[idx]} onClick={props.updateRooms}>
-                {currentRooms[idx]}
-                </button>
+                  <button key={currentRooms[idx]} value={currentRooms[idx]} onClick={props.updateRooms}>
+                  {currentRooms[idx]}
+                  </button>
             );
             })}
+        </div>
       </>
     );
   };
